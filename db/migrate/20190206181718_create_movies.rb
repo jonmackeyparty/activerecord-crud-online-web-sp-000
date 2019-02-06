dc0f28f2ad95c1e3e3ba8cde3d18f8b36959c135
Jon Mackey <jon@jonmackey.party>
2019-02-06 18:32:42 +0000

    Automatically backed up by Learn

--- db/migrate/20190206181718_create_movies.rb
+++ db/migrate/20190206181718_create_movies.rb
@@ -1,9 +1,5 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :movie, :title, :string 
-    add_column :movie, :release_date, :integer
-    add_column :movie, :director, :string 
-    add_column :movie, :lead, :string 
-    add_column :movie, :in_theaters, :boolean 
+    
   end
 end
