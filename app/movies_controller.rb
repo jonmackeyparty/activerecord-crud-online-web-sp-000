659220fd0f8bf88335bd0b317a2950964e53bddf
Jon Mackey <jon@jonmackey.party>
2019-02-06 19:19:37 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,7 +47,9 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.all.find_by(:id => 0).title
+  movie = Movie.all.find_by(:id => 0)
+  binding.pry
+  movie.title
 end
 
 def can_find_by_multiple_attributes
